a77241c1f5298013333d926c0ed23356b75d2a25
andriichenkolena <andriichenkoolena@gmail.com>
2017-07-19 16:30:41 +0300

    Commit to test branch before tag

--- ConsoleApplication3/Program.cs
+++ ConsoleApplication3/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World before the branch!");
+            Console.WriteLine("Hello World into the new test branch!");
 
             // Keep the console window open in debug mode.
             Console.WriteLine("Press any key to exit.");
